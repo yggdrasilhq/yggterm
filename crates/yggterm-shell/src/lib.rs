d33ce702a4e474b85e9f013360546c919833901a
Avikalpa Kundu <avi@gour.top>
2026-04-08 15:42:29 +0530

    Split yggui contract from yggterm shell

---
+++ crates/yggterm-shell/src/lib.rs
@@ -0,0 +1,11 @@
+#![recursion_limit = "256"]
+
+mod app_capture;
+mod shell;
+mod terminal_protocol;
+mod terminal_themes;
+mod window_icon;
+
+pub use shell::{
+    PendingUpdateRestart, ShellBootstrap, initial_server_sync, launch_shell, warm_daemon_start,
+};
